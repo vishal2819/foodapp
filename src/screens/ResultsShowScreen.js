fc3dcb957eb6fedaed6abb566c9ca675500e9fa2
Vishal Ramteke <vishalramteke99@gmail.com>
2024-02-20 20:41:42 +0530

    first commit

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,45 @@
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+
+import yelp from "../api/yelp";
+
+const ResultsShowScreen = ({ route }) => {
+  const [result, setResult] = useState(null);
+  const id = route.params.id;
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
+
+  return (
+    <View>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300,
+    marginVertical: 5,
+    borderRadius: 4,
+  },
+});
+
+export default ResultsShowScreen;
